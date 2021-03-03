7534a944f5f07489b6235c85007660eccf0baee0
qkrtndh <qkrtndh5@naver.com>
2021-03-03 16:24:55 +0900

    api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){//입력된 파라미터를 name키에 name value로 저장
         model.addAttribute("name",name);//모델에 name키에 name 값으로 저장
         return "hello-template";//리턴
+    }
+
+    @GetMapping("hello-string")//get방식으로 hello-string접속
+    @ResponseBody //html이 아닌 http의 body에 직접 내용 주입
+    public String helloString(@RequestParam("name") String name){//입력된 파라미터를 name키에 name value로 저장
+        return "hello" + name;//리턴
+    }
+
+    @GetMapping("hello-api")//get방식으로 hello-api접속
+    @ResponseBody //html이 아닌 http의 body에 직접 내용 주입
+    public  Hello helloApi(@RequestParam("name") String name){//입력된 파라미터를 name키에 name value로 저장
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{//클래스 내부에서 사용할 수 있는 클래스 객체
+        private  String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
