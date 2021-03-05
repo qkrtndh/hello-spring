3bfdeb52f47c41cad90b5c9275e064666405a9a8
qkrtndh <qkrtndh5@naver.com>
2021-03-05 15:48:48 +0900

    회원 목록 출력

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,10 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.swing.*;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,5 +31,11 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
